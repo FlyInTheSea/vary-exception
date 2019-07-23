4b4228c97a1c036b9ee4e076a038c217aee30a6d
杨益民 <744541711@qq.com>
2019-07-23 11:22:19 +0800

    测试 引入

--- src/Constants/ExceptionCodeConstants.php
+++ src/Constants/ExceptionCodeConstants.php
@@ -16,4 +16,6 @@ class ExceptionCodeConstants
     const file_encode_error_code_msg = '文件编码格式错误';
 
     const positive_integer_needed_code = 1;
+
+    const positive_integer_needed_msg = '必须正整数';
 }
--- src/Exceptions/VaryException.php
+++ src/Exceptions/VaryException.php
@@ -8,37 +8,13 @@
 
 namespace VaryException\Exceptions;
 
-
 use Exception;
-use Throwable;
 
+/**
+ * 并非所有的异常都集成此 父类
+ * Class VaryException
+ * @package VaryException\Exceptions
+ */
 class VaryException extends Exception
 {
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
-    {
-        $this->record();
-
-        parent::__construct($message, $code, $previous);
-    }
-
-
-    /**
-     * 如果抛出一个错误
-     *
-     * 下面的逻辑是往数据库里写日志
-     *
-     * 但是如果写日志的时候又抛出异常 (不论捕捉未捕捉 因为 调用 construct 的时候就已经有写日志的行为了)
-     *
-     * 则会进入死循环
-     *
-     * 想到的两种方法解决
-     * 1.  将写数据库相关的异常单独处理
-     * 2.  不采取写入数据库的行为  写入文件 然后使用进程单独对文件进行解析 写入到表
-     *
-     * 决定采用写入文件的形式
-     */
-    public function record() : void
-    {
-        
-    }
 }
