7fdc843acec704f8e142fc22770460e0cc116dfb
杨益民 <744541711@qq.com>
2019-07-23 16:13:50 +0800

    添加说明

---
+++ src/Record/RecordException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhu
+ * Date: 7/23/19
+ * Time: 4:05 PM
+ */
+
+namespace VaryException\Record;
+
+
+use Throwable;
+
+class RecordException
+{
+    public function main(Throwable $throwable)
+    {
+
+    }
+}
